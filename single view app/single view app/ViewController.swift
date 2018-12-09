4394b454c38c07d5c32f564d6f286f2a196fe9d5
xgqfrms <xgqfrms@outlook.com>
2018-12-09 17:54:42 +0800

    let & unicode & emoji

--- single view app/single view app/ViewController.swift
+++ single view app/single view app/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var 你好 = "hello 😀";
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print(你好);
     }
 }
+
 
